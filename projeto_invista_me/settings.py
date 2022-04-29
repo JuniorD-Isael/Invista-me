5adb3575eb72ca650cf1fe279d0b8f5830dcb3e2
JuniorD-Isael <104312466+JuniorD-Isael@users.noreply.github.com>
2022-04-29 20:59:12 -0300

    configuradas variáveis de ambiente

--- projeto_invista_me/settings.py
+++ projeto_invista_me/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+from decouple import config
 import whitenoise
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-((-!ucv=ggs&c1e-d5^dwiv=vjb+5%+cimh^y^^ot_!dann*js'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
